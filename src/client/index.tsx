f7b428c9be4ae756f4033871d0a5c5d706da1d73
swojtyczka <babtisedinfire4021@gmail.com>
2024-01-08 02:32:28 +0100

    Added join and leave events (no game state yet) (api)

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import axios from 'axios';
 
 const container = document.getElementById('app');
 const root = createRoot(container!);
+
+axios.defaults.withCredentials = true;
 
 root.render(
 	<React.StrictMode>
